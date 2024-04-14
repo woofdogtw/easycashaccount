18a3ac7f70d7dbbe50613ba43c1071cd63f489c1
Chien-Hong Chan <woofdogtw@hotmail.com>
2024-04-14 14:46:20 +0800

    Update code version.

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "tw.idv.woofdog.easycashaccount"
         minSdk = 29
         targetSdk = 34
-        versionCode = 21
+        versionCode = 22
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
